2e15c9f4efab7d50e4cd13701bc0d1a4775e0cab
Gunawan Nur Ahmad <54525132+Gunawanart@users.noreply.github.com>
2020-07-12 12:05:30 +0700

    fixing some bugs

--- memory-game/script.js
+++ memory-game/script.js
@@ -2,28 +2,58 @@
 
 let hasFlipped = false;
 let firstcard, secondcard;
+let arr =  []
 function flip() {
-    this.classList.toggle('flip')
-    if(!hasFlipped) {
-        hasFlipped = true;
-        firstcard = this;
-    } else {
-        hasFlipped  = false;
-        secondcard = this;
-    }
-    if(firstcard.getAttribute('data-value') === secondcard.getAttribute('data-value')) {
-        console.log('matched')
-        firstcard.removeEventListener('click', flip)
-        secondcard.removeEventListener('click', flip)
-        firstcard = undefined;
-        secondcard = undefined;
+    // if(firstcard === this || secondcard === this) {
+    //     this.classList.toggle('flip')
+    //     return;
+    // }
+    
+    // this.classList.toggle('flip')
+    // if(!hasFlipped) {
+    //     hasFlipped = true;
+    //     firstcard = this;
+    // } else {
+    //     hasFlipped  = false;
+    //     secondcard = this;
+    // }
+    
+    // if(firstcard.getAttribute('data-value') === secondcard.getAttribute('data-value')) {
+    //     console.log('matched')
+    //     firstcard.removeEventListener('click', flip)
+    //     secondcard.removeEventListener('click', flip)
         
-    } else {
-        setTimeout(()=> {
-            firstcard.classList.remove('flip')
-            secondcard.classList.remove('flip')
-        }, 1500)
+    // } else {
+    //     setTimeout(()=> {
+    //         firstcard.classList.remove('flip')
+    //         secondcard.classList.remove('flip')
+    //     }, 3500)
+    // }
+    this.classList.toggle('flip')
+    
+    if(this === arr[0] || this === arr[1]) {
+        this.classList.toggle('flip')
+        return;
     }
+
+    if(arr.length >= 2) {
+        arr[0].classList.remove('flip')
+        arr[1].classList.remove('flip')
+        arr = []
+    }
+    arr.push(this)
+    if(arr[0].getAttribute('data-value') === arr[1].getAttribute('data-value')) {
+        arr[0].removeEventListener('click', flip)
+        arr[1].removeEventListener('click', flip)
+        arr = []
+    // } else {
+    //     setTimeout(()=> {
+    //         arr[0].classList.remove('flip')
+    //         arr[1].classList.remove('flip')
+    //     },1500)
+    }
+    
+    
 }
 
 
