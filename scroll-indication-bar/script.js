af5cf1139ec527c75640f93504319e9d9fd14d56
Gunawan Nur Ahmad <54525132+GunawanAhmad@users.noreply.github.com>
2021-05-20 22:33:57 +0700

    add scroll indication

---
+++ scroll-indication-bar/script.js
@@ -0,0 +1,10 @@
+let scrollIndication = document.querySelector(".scroll-indication");
+
+window.addEventListener("scroll", () => {
+  let scrollTop = window.scrollY;
+  let docHeight = document.body.offsetHeight;
+  let winHeight = window.innerHeight;
+  let scrollPercent = scrollTop / (docHeight - winHeight);
+  let scrollPercentRounded = Math.round(scrollPercent * 100);
+  scrollIndication.style.width = scrollPercentRounded + "%";
+});
